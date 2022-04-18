13c052b90b0be67ce0cd0014f6ad519330dc0055
BootyAss <panda_canniball@mail.ru>
2022-04-18 17:13:15 +0300

    changed methods from get to post

--- terminal/config/routes.rb
+++ terminal/config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/journal', to: 'actions#index'
-  get '/enter', to: 'actions#enter'
-  get '/exit', to: 'actions#exit'
-  # post '/enter', to: 'actions#entry'
-  # post '/exit', to: 'actions#exit'
+  
+  post '/enter', to: 'actions#enter'
+  post '/exit', to: 'actions#exit'
 end
