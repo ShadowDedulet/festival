25e86f76230787eaceb2dbea1245232121027d57
shadowdedulet <shadow_dedulet@mail.ru>
2022-04-20 02:15:53 +0300

    added 2 new services

---
+++ data_api/app/services/purchase_service.rb
@@ -0,0 +1,17 @@
+# Evula Andzhey
+class PurchaseService
+  def initialize(params)
+    @params = params
+  end
+
+  def call
+    reservation = Reserve.find_by_id(@params[:reservation_id])
+
+    return { result: false, err: 'Reservation not valid', status: 406 } unless reservation
+
+    reservation.ticket.sold!
+    reservation.delete
+
+    { result: true, status: 200 }
+  end
+end
