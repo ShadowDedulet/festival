c887d1190594823016eae90e38283738ca26e9a5
shadowdedulet <shadow_dedulet@mail.ru>
2022-04-19 03:14:23 +0300

    added scope; fixed enum

--- terminal/app/models/action.rb
+++ terminal/app/models/action.rb
@@ -1,3 +1,5 @@
 class Action < ApplicationRecord
-  enum action: { exit: 0, enter: 1 }
+  scope :last_action, -> { where(ticket_id: @ret[:ticket_id], status: true).order('created_at DESC').first }
+
+  enum action_type: { exit: 0, enter: 1 }
 end
