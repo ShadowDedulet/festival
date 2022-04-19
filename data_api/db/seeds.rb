8f865334cf64e4021e62bebd5dfddbef3e0a969b
shadowdedulet <shadow_dedulet@mail.ru>
2022-04-20 02:15:20 +0300

    added seeds to data

--- data_api/db/seeds.rb
+++ data_api/db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+3.times do |i|
+  Event.create(
+    name: "event_#{i}",
+    date_start: (i * 2).days.from_now,
+    date_end: (i * 2 + 1).days.from_now
+  )
+end
+
+events = Event.all
+
+events.each do |e|
+  200.times do |i|
+    is_category_0 = i < 150
+    Ticket.create(
+      ticket_type: is_category_0 ? 0 : 2,
+      status: 0,
+      start_price: is_category_0 ? 1000 : 2000,
+      event_id: e.id
+    )
+  end
+end
