fb800b6e0d43baea81670fd69e3a36e2cb2268e6
shadowdedulet <shadow_dedulet@mail.ru>
2022-04-19 15:58:46 +0300

    Base work done

--- terminal/app/services/fetch_service.rb
+++ terminal/app/services/fetch_service.rb
@@ -4,7 +4,7 @@ require 'net/http'
 # Сервис для запроса по uri
 module FetchService
   def self.call(url, query_h = nil)
-    uri = URI.(url)
+    uri = URI.call(url)
     uri.params = query_h if query_h
 
     res = Net::HTTP.get_respone(uri)
