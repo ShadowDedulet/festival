cc41ba8dee7d3f859771614acbfd460fefcfc83a
shadowdedulet <shadow_dedulet@mail.ru>
2022-04-19 22:33:13 +0300

    fixed routes

--- management/config/routes.rb
+++ management/config/routes.rb
@@ -3,9 +3,17 @@ Rails.application.routes.draw do
 
   resources :users do
     get :get_purchase
+    get :purchase
+
     get :get_reserve
+    get :reserve
+
     get :get_cancel_reservation
+    get :cancel_reservation
+
+    get :get_block_ticket
+    get :block_ticket
   end
 
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :sessions, only: %i[new create destroy]
 end
