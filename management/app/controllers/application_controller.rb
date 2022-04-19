a5cd01efd2647a7e9b6a12bbc7f18945f38d0dc3
shadowdedulet <shadow_dedulet@mail.ru>
2022-04-19 22:07:02 +0300

    fixed current_user method

--- management/app/controllers/application_controller.rb
+++ management/app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :require_login
 
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
   def require_login
     redirect_to new_session_path unless session.include? :user_id
   end
--- management/app/helpers/application_helper.rb
+++ management/app/helpers/application_helper.rb
@@ -5,10 +5,6 @@ module ApplicationHelper
     success: 'alert-success'
   }
 
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
   def flash_messages(type)
     content_tag :div, flash[type], class: "alert #{FLASH_TYPES[type]}" if flash[type].present?
   end
