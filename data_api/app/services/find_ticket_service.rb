89ceafad199e03eeb17728f16870dd58e208fe51
shadowdedulet <shadow_dedulet@mail.ru>
2022-04-20 18:43:30 +0300

    little tweak

--- data_api/app/services/find_ticket_service.rb
+++ data_api/app/services/find_ticket_service.rb
@@ -1,21 +1,17 @@
 class FindTicketService
   attr_reader :ticket_id
-  REQUEST_URL = 'http://management:3000/users'.freeze
   
   def initialize(ticket_id)
     @ticket_id = ticket_id
   end
 
   def call
-    ticket = Ticket.find(ticket_id)
-    response = HTTP.get(REQUEST_URL, params: { id: ticket.user_id })
+    ticket = Ticket.find(@ticket_id)
+    
+    # raise ActiveRecord::RecordNotFound if ticket.status != 2
   rescue ActiveRecord::RecordNotFound
-    { result: false, error: 'Ticket not found', status: 406 }
-  rescue HTTP::Error => err
-    { result: false, error: err, status: 503 }
+    { result: false, error: 'Ticket not found/active', status: 406 }
   else
-    return { result: false, status: 503 } unless response.status == 200
-    user = JSON.parse(response.body)
-    { id: user['id'], fio: user['fio'], status: 200 }
+    { result: true, user_id: ticket.user_id, ticket_type: ticket.ticket_type, status: 200 }
   end
 end
