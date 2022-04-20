15a5d111303c6d96323c81df3e7af8cfda16506e
Ivan Konovalov <thejadeonwot@gmail.com>
2022-04-20 18:19:13 +0300

    clean controller

--- data_api/app/services/purchase_ticket_service.rb
+++ data_api/app/services/purchase_ticket_service.rb
@@ -10,7 +10,7 @@ class PurchaseTicketService
     return { result: false, err: 'Reservation not valid', status: 406 } unless reservation
 
     reservation.ticket.sold!
-    reservation.delete
+    #reservation.delete
 
     { result: true, status: 200 }
   end
